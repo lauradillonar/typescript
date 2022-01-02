75075dd393704f541e83bb15202af3dac9fe7099
lauradillonar <laura.dillon.ar@gmail.com>
2022-01-02 09:34:06 -0300

    Update app.ts: Ejemplo Aeroplano en TypeScript

--- actividad003/output/app.js
+++ actividad003/output/app.js
@@ -1,2 +1,93 @@
 "use strict";
-console.log("hola");
+class Turbina {
+    constructor(n) {
+        this.numTurbinas = 0;
+        this.numTurbinas = n;
+    }
+    ToString() {
+        return this.numTurbinas + " Turbinas";
+    }
+}
+class Cubierta {
+    constructor(pCabinaTripulacion, pCabinaVuelo, pSistemaEmergencia, pTanquesCombustible, pPuertasSalida) {
+        this.cabinaTripulacion = false;
+        this.cabinaVuelo = false;
+        this.sistemaEmergencia = false;
+        this.numTanquesCombustible = 0;
+        this.numPuertasSalidas = 0;
+        this.cabinaTripulacion = pCabinaTripulacion;
+        this.cabinaVuelo = pCabinaVuelo;
+        this.sistemaEmergencia = pSistemaEmergencia;
+        this.numTanquesCombustible = pTanquesCombustible;
+        this.numPuertasSalidas = pPuertasSalida;
+    }
+    ToString() {
+        let mensaje = " Cubierta compuesta de: ";
+        if (this.cabinaVuelo)
+            mensaje += " Cubierta de vuelo, ";
+        if (this.cabinaTripulacion)
+            mensaje += " Cubierta de tripulación, ";
+        if (this.sistemaEmergencia)
+            mensaje += " Sistema de emergencia, ";
+        mensaje += this.numTanquesCombustible + " Tanques de combustible, ";
+        mensaje += this.numPuertasSalidas + " Puertas de salida.";
+        return mensaje;
+    }
+}
+class Helice {
+    constructor(n) {
+        this.numHelices = 0;
+        this.numHelices = n;
+    }
+    ToString() {
+        return this.numHelices + " Hélices";
+    }
+}
+class TrenDeAterrizaje {
+    constructor(pRetractilFijo, pNeumaticos, pAmortiguadores) {
+        this.retractilFijo = false;
+        this.numNeumaticos = 0;
+        this.numAmortiguadores = 0;
+        this.retractilFijo = pRetractilFijo;
+        this.numNeumaticos = pNeumaticos;
+        this.numAmortiguadores = pAmortiguadores;
+    }
+    ToString() {
+        let mensaje = " Tren de Aterrizaje compuesto por: ";
+        if (this.retractilFijo)
+            mensaje += " Retractil fijo, ";
+        mensaje += this.numNeumaticos + " Numáticos, ";
+        mensaje += this.numAmortiguadores + " Amortiguadores, ";
+        return mensaje;
+    }
+}
+class Alas {
+    constructor(pAlasFrontales, pAlasPosteriores) {
+        this.numAlasFrontales = 0;
+        this.numAlasPosteriores = 0;
+        this.numAlasFrontales = pAlasFrontales;
+        this.numAlasPosteriores = pAlasPosteriores;
+    }
+    ToString() {
+        let mensaje = "";
+        mensaje += this.numAlasFrontales + " Alas frontales, ";
+        mensaje += this.numAlasPosteriores + " Alas posteriores, ";
+        return mensaje;
+    }
+}
+class Aeroplano {
+    constructor(phelice, pTrenAterrizaje, pAlas, pCubierta) {
+        this.helice = phelice;
+        this.trenAterrizaje = pTrenAterrizaje;
+        this.alas = pAlas;
+        this.cubierta = pCubierta;
+    }
+    ToString() {
+        let mensaje = "Aeroplano compuesto por: ";
+        mensaje += this.helice.ToString();
+        mensaje += this.alas.ToString();
+        mensaje += this.trenAterrizaje.ToString();
+        mensaje += this.cubierta.ToString();
+        return mensaje;
+    }
+}
--- actividad003/src/app.ts
+++ actividad003/src/app.ts
@@ -1,1 +1,130 @@
-console.log("hola");
+class Turbina
+{
+    private numTurbinas:number = 0;
+    public constructor(n:number)
+    {
+        this.numTurbinas = n;
+    }
+    public ToString()
+    {
+        return this.numTurbinas + " Turbinas";
+    }
+}
+
+class Cubierta
+{
+    private cabinaTripulacion:boolean = false;
+    private cabinaVuelo:boolean =false;
+    private sistemaEmergencia:boolean=false;
+    private numTanquesCombustible:number=0;
+    private numPuertasSalidas:number=0;
+
+    public constructor(pCabinaTripulacion:boolean, pCabinaVuelo:boolean, pSistemaEmergencia:boolean, pTanquesCombustible:number, pPuertasSalida:number)
+    {
+        this.cabinaTripulacion = pCabinaTripulacion;
+        this.cabinaVuelo = pCabinaVuelo;
+        this.sistemaEmergencia = pSistemaEmergencia;
+        this.numTanquesCombustible = pTanquesCombustible;
+        this.numPuertasSalidas = pPuertasSalida;
+    }
+
+    public ToString()
+    {
+        let mensaje = " Cubierta compuesta de: ";
+        if(this.cabinaVuelo)
+            mensaje += " Cubierta de vuelo, ";
+        if(this.cabinaTripulacion)
+            mensaje += " Cubierta de tripulación, ";
+        if(this.sistemaEmergencia)
+            mensaje += " Sistema de emergencia, ";
+        
+        mensaje += this.numTanquesCombustible + " Tanques de combustible, ";
+        mensaje += this.numPuertasSalidas + " Puertas de salida.";
+        return mensaje;
+    }
+}
+
+class Helice
+{
+    private numHelices: number = 0;
+    public constructor(n:number)
+    {
+        this.numHelices = n;
+    }
+    public ToString()
+    {
+        return this.numHelices + " Hélices";
+    }
+}
+
+class TrenDeAterrizaje
+{
+    private retractilFijo:boolean=false;
+    private numNeumaticos:number=0;
+    private numAmortiguadores:number=0;
+
+    public constructor(pRetractilFijo:boolean, pNeumaticos: number, pAmortiguadores: number)
+    {
+        this.retractilFijo = pRetractilFijo;
+        this.numNeumaticos = pNeumaticos;
+        this.numAmortiguadores = pAmortiguadores;
+    }
+
+    public ToString()
+    {
+        let mensaje = " Tren de Aterrizaje compuesto por: ";
+        if(this.retractilFijo)
+            mensaje += " Retractil fijo, ";
+        mensaje += this.numNeumaticos + " Numáticos, ";
+        mensaje += this.numAmortiguadores + " Amortiguadores, ";
+        return mensaje;
+    }
+}
+
+class Alas
+{
+    private numAlasFrontales: number = 0;
+    private numAlasPosteriores: number = 0;
+    
+    public constructor(pAlasFrontales: number, pAlasPosteriores: number)
+    {
+        this.numAlasFrontales = pAlasFrontales;
+        this.numAlasPosteriores = pAlasPosteriores;
+    }
+
+    public ToString()
+    {
+        let mensaje: String = "";
+        
+        mensaje += this.numAlasFrontales + " Alas frontales, ";
+        mensaje += this.numAlasPosteriores + " Alas posteriores, ";
+        return mensaje; 
+    }
+
+}
+
+class Aeroplano
+{
+    private helice: Helice;
+    private trenAterrizaje: TrenDeAterrizaje;
+    private alas:Alas;
+    private cubierta: Cubierta;
+    
+    constructor(phelice: Helice, pTrenAterrizaje:TrenDeAterrizaje, pAlas:Alas, pCubierta:Cubierta)
+    {
+        this.helice = phelice;
+        this.trenAterrizaje = pTrenAterrizaje;
+        this.alas = pAlas;
+        this.cubierta = pCubierta;
+    }
+
+    public ToString()
+    {
+        let mensaje = "Aeroplano compuesto por: ";
+        mensaje += this.helice.ToString();
+        mensaje += this.alas.ToString();
+        mensaje += this.trenAterrizaje.ToString();
+        mensaje += this.cubierta.ToString();
+        return mensaje;
+    }
+}
