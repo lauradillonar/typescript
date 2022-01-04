3652e518e4a2b858f8da77543ef58bbae12c30a8
lauradillonar <laura.dillon.ar@gmail.com>
2022-01-04 00:56:36 -0300

    Clases - modificador private - Por Makigas

---
+++ actividad011/output/clases.js
@@ -0,0 +1,19 @@
+"use strict";
+class Rectangulo {
+    constructor(ancho, alto) {
+        console.log("constructor()");
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+    area() {
+        console.log("area()");
+        return this.ancho * this.alto;
+    }
+    perimetro() {
+        return this.ancho * 2 + this.alto * 2;
+    }
+}
+let r1 = new Rectangulo(10, 15);
+let r2 = new Rectangulo(4, 3);
+console.log(r1.area());
+console.log(r2.area());
---
+++ actividad011/src/clases.ts
@@ -0,0 +1,25 @@
+class Rectangulo {
+    private ancho: number
+    private alto: number
+
+    constructor(ancho:number, alto:number){
+        console.log("constructor()")
+        this.ancho = ancho
+        this.alto = alto
+    }
+
+    area(){
+        console.log("area()")
+        return this.ancho * this.alto
+    }
+
+    perimetro(){
+        return this.ancho * 2 + this.alto * 2
+    }
+}
+
+let r1 = new Rectangulo(10,15)
+let r2 = new Rectangulo(4,3)
+
+console.log(r1.area())
+console.log(r2.area())
