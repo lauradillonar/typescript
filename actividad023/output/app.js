e46d6750419390eb8f270c74fcd84cc0f8543a25
lauradillonar <laura.dillon.ar@gmail.com>
2022-01-05 12:50:54 -0300

    instanceof y las guardas - Por Makigas

--- actividad023/output/app.js
+++ actividad023/output/app.js
@@ -1,11 +1,34 @@
 "use strict";
+function esUnaGeometria(x) {
+    return x.lados && x.pintar;
+}
+/*
+function esUnCuadrado(x:any): x is Cuadrado {
+    return x.lados && x.lados == 4 && x.pintar && x.lado
+}
+*/
+function esUnCuadrado(x) {
+    return esUnaGeometria(x) && x.lado;
+}
+function esUnTriangulo(x) {
+    return esUnaGeometria(x) && x.base && x.altura;
+}
 function procesar(g) {
-    if (g.lados == 4) {
-        let cuadrado = g;
-        cuadrado.lado;
+    if (esUnCuadrado(g)) {
+        g.lado;
     }
-    else if (g.lados == 3) {
-        let triangulo = g;
-        triangulo.altura;
+    else if (esUnTriangulo(g)) {
+        g.base;
     }
 }
+/*
+function procesar(g: Geometria){
+    if(g.lados == 4){
+        let cuadrado = g as Cuadrado
+        cuadrado.lado
+    } else if(g.lados == 3){
+        let triangulo = g as Triangulo
+        triangulo.altura
+    }
+}
+*/ 
--- actividad023/src/app.ts
+++ actividad023/src/app.ts
@@ -1,5 +1,6 @@
 interface Geometria {
     lados: number
+    pintar():void
 }
 
 interface Triangulo extends Geometria {
@@ -11,6 +12,32 @@ interface Cuadrado extends Geometria {
     lado: number
 }
 
+function esUnaGeometria(x: any): x is Geometria {
+    return x.lados && x.pintar
+}
+/*
+function esUnCuadrado(x:any): x is Cuadrado {
+    return x.lados && x.lados == 4 && x.pintar && x.lado
+}
+*/
+
+function esUnCuadrado(x:any): x is Cuadrado{
+    return esUnaGeometria(x)&&(x as any).lado
+}
+
+function esUnTriangulo(x:any):x is Triangulo {
+    return esUnaGeometria(x)&&(x as any).base && (x as any).altura
+}
+
+function procesar(g: Geometria){
+    if(esUnCuadrado(g)){
+        g.lado
+    } else if(esUnTriangulo(g)){
+        g.base 
+    }
+}
+
+/*
 function procesar(g: Geometria){
     if(g.lados == 4){
         let cuadrado = g as Cuadrado
@@ -20,3 +47,4 @@ function procesar(g: Geometria){
         triangulo.altura
     }
 }
+*/
